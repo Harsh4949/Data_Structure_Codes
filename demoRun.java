7e92a599b4d473e6d8fded9c8ebd164c0cadc03c
Harshvardhan Patil <patilharshvardhan4949@gmail.com>
2025-03-28 00:19:43 +0530

    Remove unused import in PQInJCF.java and add demoRun.java to demonstrate PriorityQueue functionality

---
+++ demoRun.java
@@ -0,0 +1,19 @@
+import java.util.PriorityQueue;
+
+public class demoRun {
+
+    public static void main(String[] args) {
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        int [] arr = {-3, 0, 2, 1};
+
+        for(int i : arr){
+            pq.add(i);
+        }
+
+        
+
+    }
+    
+}
