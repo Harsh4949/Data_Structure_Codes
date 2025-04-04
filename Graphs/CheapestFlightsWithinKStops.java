d0af6b1a3ba4b4d3fd4cf170fbb62bd34adc756b
Harshvardhan Patil <patilharshvardhan4949@gmail.com>
2025-04-05 00:44:14 +0530

    Add logic diagrams for CheapestFlightsWithinKStops and enhance class structure

--- Graphs/CheapestFlightsWithinKStops.java
+++ Graphs/CheapestFlightsWithinKStops.java
@@ -1,10 +1,24 @@
 package Graphs;
 
-public class CheapestFlightsWithinKStops {
+public class CheapestFlightsWithinKStops {  // Useses DJIltra Algo here
 
-    
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int src,int dest,int wt){
+            this.src=src;
+            this.dest=dest;
+            this.wt=wt;
+        }
+    }
     
     public static void main(String[] args) {
-        
+
+        int n = 4;
+        int flights[][] = {{0,1,100}, {1,2,100}, {2,0,100}, {1,3,600}, {2,3,200}};
+        int src = 0, dst = 3, k = 1;
+
     }
 }
