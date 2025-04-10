c71637e4a6fc406f831b41d36782522620e7ef85
Harshvardhan Patil <patilharshvardhan4949@gmail.com>
2025-04-10 23:42:44 +0530

    Refactor ClimbingStairs class methods for clarity and consistency in output formatting

--- DynamicProgramming/Type_1/ClimbingStairs.java
+++ DynamicProgramming/Type_1/ClimbingStairs.java
@@ -20,29 +20,56 @@ public class ClimbingStairs {
 
     public static int countWaysDp(int n,int ways[]){      //same logic as fbinanci see thier code and images for understnading
 
-        if (n==0) {
-            return 1;
-        }
-
-        if (n<0) {
+        if (n<0) {                 
             return 0;
         }
 
-        if (ways[n]!=-1) {
-            return ways[n];
+        if (n==0) {
+           // System.out.println("hello");
+            return 1;
         }
 
+        if (ways[n]!=-1) {                      //memoization
+            return ways[n];
+        }
+        
+
         return ways[n]=countWaysDp(n-1, ways)+countWaysDp(n-2, ways);
+    }
+
+
+    public static int countWaysDpTab(int n){
+
+        int dp[]= new int[n+1];
+        dp[0]=1;
+
+        for (int i = 1; i <=n; i++) {
+            
+            if (i==1) {   
+                dp[i]=dp[i-1]+0;  //1-2 = -1 wichi is false to add 0 for understnading
+
+            } else {
+                
+                dp[i]=dp[i-1]+dp[i-2];
+            }
+        }
+
+        return dp[n];
+
     }
     
     public static void main(String[] args) {
         
         int n=5;
-        System.out.println(countWays(5));
+        System.out.println("simple Recursion :"+countWays(5));
 
         int[] ways= new int[n+1];
         Arrays.fill(ways, -1);
-        System.out.println(countWaysDp(n, ways));
+       // ways[0]=1; also done
+
+        System.out.println("Memoization : "+countWaysDp(n, ways));
         //System.out.println(ways[n]);
+
+        System.out.println("Tabulation: "+countWaysDpTab(n));
     }
 }
