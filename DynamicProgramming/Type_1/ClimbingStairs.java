68d5dab317bbee07592a2b5bce626cd1b6702620
Harshvardhan Patil <patilharshvardhan4949@gmail.com>
2025-04-10 23:07:45 +0530

    Add ClimbingStairs class implementation with recursive and dynamic programming methods, and include visual representation

--- DynamicProgramming/Type_1/ClimbingStairs.java
+++ DynamicProgramming/Type_1/ClimbingStairs.java
@@ -1,8 +1,48 @@
 package DynamicProgramming.Type_1;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class ClimbingStairs {
+
+    public static int countWays(int n) {    //normal recursion draw tree O(2^N)
+        
+        if (n==0) {
+            return 1;
+        }
+
+        if (n<0) {
+            return 0;
+        }
+
+        return countWays(n-1)+countWays(n-2);
+    }
+
+    public static int countWaysDp(int n,int ways[]){      //same logic as fbinanci see thier code and images for understnading
+
+        if (n==0) {
+            return 1;
+        }
+
+        if (n<0) {
+            return 0;
+        }
+
+        if (ways[n]!=-1) {
+            return ways[n];
+        }
+
+        return ways[n]=countWaysDp(n-1, ways)+countWaysDp(n-2, ways);
+    }
     
     public static void main(String[] args) {
         
+        int n=5;
+        System.out.println(countWays(5));
+
+        int[] ways= new int[n+1];
+        Arrays.fill(ways, -1);
+        System.out.println(countWaysDp(n, ways));
+        //System.out.println(ways[n]);
     }
 }
