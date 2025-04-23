829ace44a87e1930996295f6fa439630c489ee0a
Harshvardhan Patil <patilharshvardhan4949@gmail.com>
2025-04-23 22:47:08 +0530

    Refactor LCS method to improve readability and add base case handling

--- DynamicProgramming/Type_3/LCS.java
+++ DynamicProgramming/Type_3/LCS.java
@@ -2,9 +2,23 @@ package DynamicProgramming.Type_3;
 
 public class LCS {      //Longest Common Subsequence 
     
-    public static int LCS(String str1,String str2,int n,int m) {
+    public static int LCS(String str1,String str2,int n,int m) { // See Logic in LCSLogic.png image
         
+        if (n==0 || m==0) {
+            return 0;
+        }
+
+        if (str1.charAt(n-1)==str2.charAt(m-1)) { 
+            // if last two char are same then reduce the size of String By 1
+            //And Add 1 in Answer 
+            return LCS(str1, str2, n-1, m-1)+1;
+        } else {
+            // if not same then Make Two Call bye Redusing size bye Each One
+            return Math.max(LCS(str1, str2, n-1, m), LCS(str1, str2, n, m-1));  
+        }
     }
+
+    
 
     public static void main(String[] args) {
         
