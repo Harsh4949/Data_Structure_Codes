d454382f9e4bbd2d16145d2d1b6c6d358e0e7963
Harshvardhan Patil <patilharshvardhan4949@gmail.com>
2025-04-23 22:41:13 +0530

    Add LCS class implementation and logic diagrams for Longest Common Subsequence

---
+++ DynamicProgramming/Type_3/LCS.java
@@ -0,0 +1,16 @@
+package DynamicProgramming.Type_3;
+
+public class LCS {      //Longest Common Subsequence 
+    
+    public static int LCS(String str1,String str2,int n,int m) {
+        
+    }
+
+    public static void main(String[] args) {
+        
+        String str1="abcde";
+        String str2="ace";
+
+        System.out.println(LCS(str1, str2, str1.length(), str2.length()));
+    }
+}
