6dd9c8e4ecce96c51ab18fccbb8ca3d5efce36e9
Harshvardhan Patil <patilharshvardhan4949@gmail.com>
2025-06-11 19:37:08 +0530

    Add MCM dry run image and enhance comments in MatrixChainMultiplication class

--- DynamicProgramming/Type_5/MatrixChainMultiplication.java
+++ DynamicProgramming/Type_5/MatrixChainMultiplication.java
@@ -1,4 +1,7 @@
+import java.util.Arrays;
 
+import DynamicProgramming.Type_2.a0_1Knapsack;
+import DynamicProgramming.Type_3.*;
 public class MatrixChainMultiplication {
 
     public static int mcm(int[] arr,int i,int j) {
@@ -11,12 +14,12 @@ public class MatrixChainMultiplication {
 
         for(int k=i;k<=j-1;k++){
                                           //                    a  X  b               c  X  d     == result MAtrix(a,d)
-            int cost1=mcm(arr, i, k);   // Ai..Ak     ->  arr[i-1] X arr[i](A), arr[k-1] X arr[k] == arr[i-1] X arr[k]
-            int cost2=mcm(arr, k+1, j); // Ak+1....Kj ->  arr[k]   X arr[k+1] , arr[j-1] X arr[j] == arr[k] X arr[j]
+            int cost1=mcm(arr, i, k);   // Ai..Ak     ->  arr[i-1] X arr[i](A), arr[k-1] X arr[k] == arr[i-1] X arr[k]    // left tree call 
+            int cost2=mcm(arr, k+1, j); // Ak+1....Kj ->  arr[k]   X arr[k+1] , arr[j-1] X arr[j] == arr[k] X arr[j]        //right tree call
             
             int cost3=arr[i-1]*arr[k]*arr[j];    // cost= a*b*d         
             
-            int finalresult=cost1+cost2+cost3;
+            int finalresult=cost1+cost2+cost3;  // see dry run to more understans "c:\Users\Harsh\Downloads\Phone Link\MCM_DryRun.jpg" 
 
             ans=Math.min(ans, finalresult);
 
@@ -24,13 +27,54 @@ public class MatrixChainMultiplication {
 
         return ans;
     }
+
+    public static int mcmMemo(int arr[],int i,int j,int dp[][]){ // see recurtion call tree for understand
+
+        if (i==j) {
+            return 0;
+        }
+
+        if (dp[i][j]!=-1) {
+            return dp[i][j];
+        }
+
+        int ans=Integer.MAX_VALUE;
+
+        for(int k=i;k<=j-1;k++){
+
+            int cost1 = mcmMemo(arr, i, k,dp);
+            // Ai..Ak     ->  arr[i-1] X arr[i](A), arr[k-1] X arr[k] == arr[i-1] X arr[k]
+            int cost2 =mcmMemo(arr, k+1, j, dp);
+           // Ak+1....Kj ->  arr[k]   X arr[k+1] , arr[j-1] X arr[j] == arr[k] X arr[j] 
+
+           int cost3 =arr[i-1]*arr[k]*arr[j]; //a*b*d
+
+           int finalCostOprations=cost1+cost2+cost3;
+
+           ans=Math.min(ans, finalCostOprations);
+        }
+
+        return dp[i][j]=ans;
+    }
+
+    public static void mcmTab(int[] arr) {
+        
+    }
     
     public static void main(String[] args) {
 
         int arr[]={1,2,3,4,3};  // A=1X2 , B= 2X3 ....
         int n= arr.length;
         System.out.println(mcm(arr, 1, n-1));
-        
+
+        int dp[][]= new int[n][n];
+
+        for(int i=0;i<n;i++)
+        Arrays.fill(dp[i], -1);
+
+        System.out.println(mcmMemo(arr, 1, n-1, dp));
+       // a0_1Knapsack.print(dp);
+
     }
 }
 
